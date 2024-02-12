65a181050f8c9b198598f3cd7fce65c40f17edb1
Dampfie93 <kevin@buesing-mail.de>
2024-02-12 04:02:03 +0100

    followup to last commit

--- alarm_manager.py
+++ alarm_manager.py
@@ -49,7 +49,7 @@ class Alarm(FileManager):
     def checkAlarm(self):
         if self.repeat == -1 and not self.active:
             return None
-        else:5 <= 5 und 125 >= 5
+        else:
             checkActive  = self.active == True
             checkTime    = self.time <= time() and self.time+120 >= time()
             #checkWeekDay = self.weekday[gmtime()[6]] == "1"
